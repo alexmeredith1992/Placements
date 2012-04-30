cec593f46bfd8b70e6dcbf7fc63a1e4c0bb9bfb0
student <student@WebArchLinuxBase1.localdomain>
2012-04-30 17:13:23 +0100

    I ammened the wrong BootStrap here is the correctly ammended one

--- Placements/grails-app/conf/BootStrap.groovy
+++ Placements/grails-app/conf/BootStrap.groovy
@@ -3,7 +3,13 @@ import placements.Status
 class BootStrap {
 
     def init = { servletContext ->
-     new code
+   new Status(code:'Applied', description:'Applied').save();
+    new Status(code:'Not Invited for Interview', description:'Not Invited for Interview').save();
+    new Status(code:'Invited for Interview', description:'Invited for Interview').save();
+    new Status(code:'Not Offered', description:'Not Offered').save();
+    new Status(code:'Offered', description:'Offered').save();
+    new Status(code:'Accepted', description:'Accepted').save();
+    new Status(code:'Rejected', description:'Rejected').save();
 
     }
     def destroy = {
